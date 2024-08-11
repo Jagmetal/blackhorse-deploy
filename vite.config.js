91636e6b4e53a11b9fa9627e75caf059ea8897b3
Jagwinder Singh <jagsingh@hotmail.co.nz>
2024-08-11 22:11:54 +1200

    updated vite config file

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/blackhorse-deploy/',
+  base: "https://jagmetal.github.io/blackhorse-deploy/",
 });
