a588f24d699f8a7dc93f8fcd833ded292ce94871
Jagwinder Singh <jagsingh@hotmail.co.nz>
2024-08-11 21:46:50 +1200

    updated browser code

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+import { BrowserRouter } from 'react-router-dom'; // Import BrowserRouter
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <browserRouter basename="/blackhorse-deploy/">
-    <App />
-    </browserRouter>
+    <BrowserRouter basename="/blackhorse-deploy/">
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
-)
+);
